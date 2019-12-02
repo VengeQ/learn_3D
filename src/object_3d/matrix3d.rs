0a71411e574ebd034bd07b7dcc74e3ed0b34642e
Balukin, Daniil <Daniil.Balukin@nexign.com>
2019-12-02 18:00:02 +1000

    Add matrix3d product

--- src/object_3d/matrix3d.rs
+++ src/object_3d/matrix3d.rs
@@ -51,11 +51,29 @@ impl Matrix3d {
         (row0, row1, row2)
     }
 
-    pub fn vector_product(&self, right:Vector3d) ->Vector3d{
-        let x = self.inner[0] * right.x()+self.inner[3]*right.y()+self.inner[6]*right.z();
-        let y = self.inner[1] * right.x()+self.inner[4]*right.y()+self.inner[7]*right.z();
-        let z = self.inner[2] * right.x()+self.inner[5]*right.y()+self.inner[8]*right.z();
-        Vector3d::new(x,y,z)
+    pub fn vector_product(&self, other: &Vector3d) -> Vector3d {
+        let x = self.inner[0] * other.x() + self.inner[3] * other.y() + self.inner[6] * other.z();
+        let y = self.inner[1] * other.x() + self.inner[4] * other.y() + self.inner[7] * other.z();
+        let z = self.inner[2] * other.x() + self.inner[5] * other.y() + self.inner[8] * other.z();
+        Vector3d::new(x, y, z)
+    }
+
+    pub fn matrix_product(&self, other: &Matrix3d) -> Matrix3d {
+        let mut result = Self::new_zero();
+        let other = other.inner;
+        result.inner[0] = self.inner[0] * other[0] + self.inner[3] * other[1] + self.inner[6] * other[2];
+        result.inner[1] = self.inner[1] * other[0] + self.inner[4] * other[1] + self.inner[7] * other[2];
+        result.inner[2] = self.inner[2] * other[0] + self.inner[5] * other[1] + self.inner[8] * other[2];
+
+        result.inner[3] = self.inner[0] * other[3] + self.inner[3] * other[4] + self.inner[6] * other[5];
+        result.inner[4] = self.inner[1] * other[3] + self.inner[4] * other[4] + self.inner[7] * other[5];
+        result.inner[5] = self.inner[2] * other[3] + self.inner[5] * other[4] + self.inner[8] * other[5];
+
+        result.inner[6] = self.inner[0] * other[6] + self.inner[3] * other[7] + self.inner[6] * other[8];
+        result.inner[7] = self.inner[1] * other[6] + self.inner[4] * other[7] + self.inner[7] * other[8];
+        result.inner[8] = self.inner[2] * other[6] + self.inner[5] * other[7] + self.inner[8] * other[8];
+
+        result
     }
 
     pub fn new_diagonal() -> Self {
@@ -68,6 +86,7 @@ impl Matrix3d {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::Rng;
 
     #[test]
     fn matrix3d_new_zero_test() {
@@ -78,7 +97,7 @@ mod tests {
     #[test]
     fn matrix3d_new_diagonal_test() {
         let m = Matrix3d::new_zero();
-        m.inner.iter().for_each(|x| assert_eq!(*x, 0.0))
+        m.inner.iter().for_each(|x| assert !(*x >= 0.0 && *x <= 1.0))
     }
 
     #[test]
@@ -121,11 +140,47 @@ mod tests {
     }
 
     #[test]
-    fn vector_product_test(){
+    fn vector_product_test() {
+        let mut rng = rand::thread_rng();
+        for _ in 0..100 {
+            let x00 = rng.gen_range(0, 100) as f64;
+            let x01 = rng.gen_range(0, 100) as f64;
+            let x02 = rng.gen_range(0, 100) as f64;
+            let x10 = rng.gen_range(0, 100) as f64;
+            let x11 = rng.gen_range(0, 100) as f64;
+            let x12 = rng.gen_range(0, 100) as f64;
+            let x20 = rng.gen_range(0, 100) as f64;
+            let x21 = rng.gen_range(0, 100) as f64;
+            let x22 = rng.gen_range(0, 100) as f64;
+
+            let v0 = rng.gen_range(0, 100) as f64;
+            let v1 = rng.gen_range(0, 100) as f64;
+            let v2 = rng.gen_range(0, 100) as f64;
+
+            let i = [x00, x01, x02, x10, x11, x12, x20, x21, x22];
+            let matrix = Matrix3d::from_array(i);
+            let vector = Vector3d::new(v0, v1, v2);
+
+            let mv_product = matrix.vector_product(&vector);
+            let manual_product = Vector3d::new(
+                v0 * x00 + v1 * x10 + v2 * x20,
+                v0 * x01 + v1 * x11 + v2 * x21,
+                v0 * x02 + v1 * x12 + v2 * x22);
+            assert_eq!(manual_product, mv_product);
+        }
         let i = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
         let matrix = Matrix3d::from_array(i);
-        let vector = Vector3d::new(2.0,3.0,6.0);
-        let mv_product = matrix.vector_product(vector);
+        let vector = Vector3d::new(2.0, 3.0, 6.0);
+        let mv_product = matrix.vector_product(&vector);
+        println!("{:?}", mv_product);
+    }
+
+    #[test]
+    fn matrix_product_test() {
+        let i = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0];
+        let m1 = Matrix3d::from_array(i);
+        let m2 = Matrix3d::from_array(i);
+        let mv_product = m1.matrix_product(&m2);
         println!("{:?}", mv_product);
     }
 }
